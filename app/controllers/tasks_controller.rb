93f29e99e2170c1412b82e25637f218a6a298349
koichiro.katayama <katayama.legal.office@gmail.com>
2019-07-20 07:01:48 +0000

    login and user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,16 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  before_action :check_user, only: [:show, :edit, :update, :destroy]
+  # correct_user
   
     def index
-        @tasks = Task.order(id: :desc).page(params[:page]).per(20)
+        # @tasks = Task.order(id: :desc).page(params[:page]).per(20)
+        # @tasks = Task.where(user_id: current_user.id).order(id: :desc).page(params[:page]).per(20)
+        # @tasks = Task.where(user: current_user).order(id: :desc).page(params[:page]).per(20)
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(20)
     end
     
     def show
-        @task = Task.find(params[:id])
     end
     
     def new
@@ -14,10 +18,16 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        # Userモデルにhas_many :tasksが書いてあることで有効
+        @task = current_user.tasks.build(task_params)
+        # @task = Task.new(task_params)
+        # @task.user_id = current_user.id
+        # 下記はTaskモデルにbelongs_to :userが書いてあることで有効
+        # @task.user = current_user
         if @task.save
          flash[:success] = 'Taskが正常に作成されました'
-         redirect_to @task
+         redirect_to root_url
+         #@task
         else
          flash.now[:danger] = 'Taskが作成されませんでした'
          render :new
@@ -25,11 +35,10 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find(params[:id])
     end
     
     def update
-      @task = Task.find(params[:id])
+      # @task.user = current_user
       if @task.update(task_params)
         flash[:success] = 'Task は正常に更新されました'
         redirect_to @task
@@ -40,7 +49,6 @@ class TasksController < ApplicationController
     end
     
     def destroy
-      @task = Task.find(params[:id])
       @task.destroy
       
       flash[:success] = 'Task は正常に削除されました'
@@ -51,6 +59,10 @@ class TasksController < ApplicationController
     
     def task_params
       params.require(:task).permit(:content, :status)
+    end
+    def check_user
+      @task = Task.find(params[:id])
+      redirect_to root_url if @task.user != current_user
     end
 
 end
